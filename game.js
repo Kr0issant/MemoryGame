343aa17f3f12b5c460c91c9f3fb3c3843fe6b5e0
Kroissant <93475036+Kr0issant@users.noreply.github.com>
2025-04-29 22:30:51 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,67 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var flashDuration = 100;
+
+var level = 0;
+var lost = false;
+var correctPattern = [];
+var userTurns = 0;
+$(".btn").click(clickHandler);
+$(document).on("keydown", function(event) {
+    if (event.key === "Enter") {
+        if (level === 0) { 
+            setTimeout(nextSequence, 500);
+        }
+        else if (lost === true) {
+            level = 0;
+            lost = false;
+            nextSequence();
+        }
+    }
+});
+
+function nextSequence() {
+    // update level to title
+    level++;
+    $("#level-title").text("Level " + level);
+    userTurns = 0;
+
+    // add random color to list
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    correctPattern.push(randomChosenColor);
+
+    // color flash animation and sound
+    $("#" + randomChosenColor).fadeOut(flashDuration).fadeIn(flashDuration);
+    new Audio("sounds/" + randomChosenColor + ".mp3").play();
+}
+
+function clickHandler(event) {
+    if (lost === false && level > 0) {
+        var userChosenColor = event.target.getAttribute("id");
+        userTurns++;
+
+        new Audio("sounds/" + userChosenColor + ".mp3").play();
+
+        // pressing animation
+        $("#" + userChosenColor).addClass("pressed")
+        setTimeout(function() { $("#" + userChosenColor).removeClass("pressed"); }, 100);
+
+        checkAnswer(userChosenColor);
+    }
+}
+
+function checkAnswer(choice) {
+    if (choice === correctPattern[userTurns - 1]) {
+        if (userTurns === level) {
+            setTimeout(nextSequence, 1000);
+        }
+    }
+    else {
+        lost = true;
+        correctPattern = [];
+        $("#level-title").text("Game Over. Press Enter to Restart");
+
+        $("body").addClass("game-over");
+        setTimeout(function() { $("body").removeClass("game-over"); }, 100);
+    }
+}
